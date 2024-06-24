7b76d2cc1e80c34ac6a4f6e54e986fc84a6591ce
natan <natanbinistilyon@gmail.com>
2024-06-24 16:59:22 +0200

    start

--- assets/app.js
+++ assets/app.js
@@ -7,4 +7,20 @@ import './bootstrap.js';
  */
 import './styles/app.css';
 
+const cursorRounded = document.querySelector('.rounded');
+const cursorPointed = document.querySelector('.pointed');
+
+
+const moveCursor = (e)=> {
+    const mouseY = e.clientY;
+    const mouseX = e.clientX;
+
+    cursorRounded.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+
+    cursorPointed.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+
+}
+
+window.addEventListener('mousemove', moveCursor)
+
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
