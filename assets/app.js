bfdb6d4eca1464f9e58546e48bcb3f8d929e2745
natan <natanbinistilyon@gmail.com>
2024-06-27 19:16:46 +0200

    html css finished

--- assets/app.js
+++ assets/app.js
@@ -7,20 +7,78 @@ import './bootstrap.js';
  */
 import './styles/app.css';
 
-const cursorRounded = document.querySelector('.rounded');
-const cursorPointed = document.querySelector('.pointed');
-
-
-const moveCursor = (e)=> {
-    const mouseY = e.clientY;
-    const mouseX = e.clientX;
-
-    cursorRounded.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-
-    cursorPointed.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-
+// Function to set the theme and update UI
+function setTheme(theme) {
+    document.body.setAttribute('data-bs-theme', theme);
+    localStorage.setItem('theme', theme);
+    var switchThemeBtn = document.getElementById('switchTheme');
+    if (switchThemeBtn) {
+        switchThemeBtn.innerHTML = theme === 'dark' ?  '<i class="bi bi-sun-fill"></i>' : '<i class="bi bi-moon-stars-fill"></i>';
+    }
+    //console.log(`Switched to ${theme} theme`);
 }
 
-window.addEventListener('mousemove', moveCursor)
+var currentTheme = localStorage.getItem('theme') || 'dark';
+setTheme(currentTheme);
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+// Event listener for the switch theme button
+var switchThemeBtn = document.getElementById('switchTheme');
+if (switchThemeBtn) {
+    switchThemeBtn.addEventListener('click', () => {
+        currentTheme = currentTheme === 'dark' ? 'light' : 'dark';
+        setTheme(currentTheme);
+    });
+}
+
+//AOS Initiliaze
+AOS.init();
+
+// Fixed Header & back to top button on Scroll
+window.addEventListener('scroll', () => {
+    // fixed header
+    const header = document.getElementById('header');
+    if (window.scrollY > 30 && !header.classList.contains('fixed-top')) {
+        header.classList.add('fixed-top');
+        document.getElementById('offcanvasNavbar').classList.add('fixedHeaderNavbar');
+    } else if (window.scrollY <= 30 && header.classList.contains('fixed-top')) {
+        header.classList.remove('fixed-top');
+        document.getElementById('offcanvasNavbar').classList.remove('fixedHeaderNavbar');
+    }
+
+    //backtotop
+    const backToTopButton = document.getElementById("backToTopButton");
+    if (window.scrollY > 400 && backToTopButton.style.display === 'none') {
+        backToTopButton.style.display = 'block';
+    } else if (window.scrollY <= 400 && backToTopButton.style.display === 'block') {
+        backToTopButton.style.display = 'none';
+    }
+});
+
+
+//jumping to top function
+function scrollToTop(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+//Testimonial Slider
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:3,
+        nav:true,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        responsive:{
+            0:{
+                items:1,
+            },
+            768:{
+                items:2,
+            },
+            1170:{
+                items:3,
+            }
+        }
+    });
+});
